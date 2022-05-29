1d929fb03887837fb49618bd9e8736ce4f50a861
jasondwood <823jwood@gmail.com>
2022-05-29 00:58:23 -0400

    first commit

---
+++ 5-29 challenge.js
@@ -0,0 +1,30 @@
+/*
+Mark and John are trying to compare their BMI, which is calculated using the following formula: 
+
+BMI = mass /height ** 2
+BMI = mass / (height*height) 
+
+mass in kg height in meters
+
+1. Store Mark and John's mass and height in variables
+
+2. Calculate both their BMIs using the formula
+
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Mark weights 78 kg and is 1.68 m tall. John weighs 92 kg and is 1.95 m tall
+
+TEST DATA 2: Mark weighs 95 kg and is 1.88 m tall. John weighs 85 kg and is 1.76 m tall
+
+*/
+
+
+let markMass1 = 78;
+let johnMass1 = 92;
+
+let markHeight1 = 1.95;
+let johnHeight1 = 1.68;
+
+
+
+
